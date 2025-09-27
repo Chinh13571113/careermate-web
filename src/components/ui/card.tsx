423d7405cb13e60c8034135e423abc1bdce07401
chinhnd23 <chinhnd23@fpt.com>
2025-09-27 15:55:03 +0700

    feat(profile): add dashboard component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,13 +1,24 @@
 "use client";
 
-interface CardProps {
+import { HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
   title?: string;
   children?: React.ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ title, children }) => {
+const Card: React.FC<CardProps> = ({
+  title,
+  children,
+  className,
+  ...props
+}) => {
   return (
-    <div className="p-4 border rounded-lg shadow-sx bg-white">
+    <div
+      className={twMerge("p-4 border rounded-lg shadow-sm bg-white", className)}
+      {...props}
+    >
       <h2 className="text-lg font-semibold mb-2">{title}</h2>
       <div>{children}</div>
     </div>
--- src/features/client/profile/ui/components/dashboard.tsx
+++ src/features/client/profile/ui/components/dashboard.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AttachedCV from "../../dashboard/ui/components/attached-cv";
 import PersonalInformation from "../../dashboard/ui/components/personal-information";
+import MyActivity from "../../dashboard/ui/components/my-activity";
 
 const Dashboard = () => {
   return (
     <div className="space-y-6">
       <PersonalInformation />
       <AttachedCV />
+      <MyActivity />
     </div>
   );
 };
